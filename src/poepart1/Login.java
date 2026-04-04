4d1fa2ee4c11bbdc33701c936f9f507376d3dae1
Michael Gouveia <ST10485005@vcconnect.edu.za>
2026-04-04 12:36:32 +0200

    Added Login class as well as added variables and constructors

---
+++ src/poepart1/Login.java
@@ -0,0 +1,18 @@
+package poepart1;
+
+public class Login {
+    
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String cellPhoneNumber;
+
+    public Login(String firstName, String lastName, String userName, String password, String cellPhoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.cellPhoneNumber = cellPhoneNumber;
+    }
+}
