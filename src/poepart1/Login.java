012059faf8ec0a8c8a095fc58d892b1000eb9068
Michael Gouveia <ST10485005@vcconnect.edu.za>
2026-04-05 11:43:53 +0200

    Added username and password validation method

--- src/poepart1/Login.java
+++ src/poepart1/Login.java
@@ -8,6 +8,7 @@ public class Login {
     private String password;
     private String cellPhoneNumber;
 
+    // Constructor
     public Login(String firstName, String lastName, String userName, String password, String cellPhoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -15,4 +16,33 @@ public class Login {
         this.password = password;
         this.cellPhoneNumber = cellPhoneNumber;
     }
+
+    public boolean checkUserName() {
+        return userName.contains("_") && userName.length() <= 5;
+    }
+    public boolean checkPasswordComplexity() {
+    boolean hasCapitalLetter = false;
+    boolean hasNumber = false;
+    boolean hasSpecialCharacter = false;
+
+    // Check length first
+    if (password.length() < 8) {
+        return false;
+    }
+
+    // Loop through each character
+    for (int i = 0; i < password.length(); i++) {
+        char ch = password.charAt(i);
+
+        if (Character.isUpperCase(ch)) {
+            hasCapitalLetter = true;
+        } else if (Character.isDigit(ch)) {
+            hasNumber = true;
+        } else if (!Character.isLetterOrDigit(ch)) {
+            hasSpecialCharacter = true;
+        }
+    }
+
+    return hasCapitalLetter && hasNumber && hasSpecialCharacter;
+}
 }
